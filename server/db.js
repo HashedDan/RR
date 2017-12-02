672bd4d24fed7fe07486d05821d37ac507bb05cd
HashedDan <dmangum@hashedhealth.com>
2017-12-02 13:42:16 -0600

    updating db connection

--- server/db.js
+++ server/db.js
@@ -1,12 +1,5 @@
-var db = require('knex')({
-  client: 'pg',
-  connection: {
-    host : process.env.DB_HOST,
-    user : process.eng.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE
-  }
-});
+var db
+
 
 module.exports = db;
 
--- server/server.js
+++ server/server.js
@@ -4,24 +4,26 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
-
+const db = require('./db');
 
 // API file for interacting with DB
-const api = require('./server/routes/api');
+// const api = require('./routes/api');
 
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
 
 // API location
-app.use('/api', api);
+// app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+	console.log(process.env.DB_USER);
+    // res.sendFile(path.join(__dirname, 'dist/index.html'));
+    db('recruits').where('recruit_id', 1).then((row) => res.send(row));
 });
 
 //Set Port
