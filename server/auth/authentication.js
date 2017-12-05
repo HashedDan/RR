a5da326ab877a75469613af50325b9dd69ac4357
HashedDan <dmangum@hashedhealth.com>
2017-12-05 00:00:09 -0600

    call it a night

---
+++ server/auth/authentication.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const pg = require('pg');
+
+function comparePasswords(userPass, dbPass) {
+	return bcrypt.compareSync(userPass, dbPass);
+}
+
+function createMember(req, res) {
+	return handleErrors(req)
+	.then(() => {
+		const salt = bcrypt.genSaltSync();
+		const hash = bcrypt.hashSync(req.body.password, salt);
+		pool.connect()
+		  .then(client => {
+		    return client.query('INSERT INTO members (member_first, member_last, member_org, member_email, member_pass) VALUES ($1, $2, $3, $4, $5)', [req.body.first, req.body.last, req.body.org, req.body.email, hash])
+		      .then(response => {
+		        client.release();
+		      })
+		      .catch(e => {
+		        client.release();
+		        res.status(400).json({status: err.message});
+		      })
+		  });
+	})
+}
