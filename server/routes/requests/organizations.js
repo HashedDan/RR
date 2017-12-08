806fe2042dee8896d27b1ac9052591e194516f7a
HashedDan <dmangum@hashedhealth.com>
2017-12-08 15:23:48 -0600

    sequelize queries working

--- server/routes/requests/organizations.js
+++ server/routes/requests/organizations.js
@@ -1,5 +1,23 @@
 const db = require('../../models/index');
 
 exports.getAll = (req, res, next) => {
-	
+	db.sequelize.query('SELECT * FROM organizations')
+		.then(response => {
+			console.log(response);
+			res.send(response.rows);
+		});
+	// pool.connect()
+	//   .then(client => {
+	//     return client.query('SELECT * FROM organizations')
+	//       .then(response => {
+	//         client.release();
+	//         res.send(response.rows)
+	//       })
+	//       .catch(e => {
+	//         client.release();
+	//         console.log(err.stack);
+	//       })
+	//   });
+
+
 }
