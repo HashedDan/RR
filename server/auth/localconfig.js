1923d533077136db8b2662ad2bb7d0c030972290
HashedDan <georgedanielmangum@gmail.com>
2017-12-17 17:27:49 -0600

    more auth stuff

---
+++ server/auth/localconfig.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const init = require('./passport');
+const authFuncs = require('./auth-functions');
+
+// const pg = require('pg');
+// const connection = 'postgres://g4:guqdTp5A2VSUjedF@localhost:5432/g4';
+
+// var client = new pg.Client(connection);
+// client.connect();
+
+init();
+
+const options = {
+	usernameField: 'email',
+	passwordField: 'password'
+};
+
+passport.use(new LocalStrategy(options, (email, password, done) => {
+	console.log(email);
+	const query = db.sequelize.query('SELECT * FROM members WHERE member_email = $1', [email], (err, results) => {
+		if (results.rows.length < 1) {
+			return done(null, false);
+		}
+		user = results.rows[0];
+		if (!authFuncs.comparePasswords(password, user.member_pass)) {
+			return done(null, false);
+		}
+		else {
+			console.log("successful authentification");
+			console.log(user);
+			return done(null, user);
+		}
+    })
+}));
+
+
+module.exports = passport;
---
+++ server/auth/passport.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+// const pg = require('pg');
+// const connection = 'postgres://g4:guqdTp5A2VSUjedF@localhost:5432/g4';
+
+// var client = new pg.Client(connection);
+// client.connect();
+const db = require('../models/index');
+
+module.exports = () => {
+
+  passport.serializeUser((user, done) => {
+    done(null, user.member_id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    const query = client.query('SELECT * FROM members WHERE  member_id = ' + id, (err, results) => {
+		user = results.rows[0];
+		return done(null, user);
+    })
+  });
+
+};
