f1eea1d4159fe90fc0ce2b72144264582bc0481b
HashedDan <georgedanielmangum@gmail.com>
2017-12-25 19:09:36 -0600

    working on front end login

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authFuncs = require('../auth/auth-functions');
+const auth = require('./requests/auth');
 const organizations = require('./requests/organizations');
 const members = require('./requests/members');
 const recruits = require('./requests/recruits');
 
-router.get('/login', authFuncs.loginRedirect);
+router.get('/login', authFuncs.loginRedirect, auth.login);
+
+router.get('/logout', authFuncs.loginRequired, auth.logout);
+
+router.get('/register', authFuncs.loginRedirect, auth.register);
 
 router.get('/organizations', organizations.getAll);
 
--- server/routes/requests/auth.js
+++ server/routes/requests/auth.js
@@ -2,7 +2,7 @@ const db = require('../../models/index');
 const passport = require('../../auth/localconfig.js');
 const bcrypt = require('bcryptjs');
 
-export.register = (req, res, next) => {
+exports.register = (req, res, next) => {
   const salt = bcrypt.genSaltSync();
   const hash = bcrypt.hashSync(req.body.password, salt);
   const query = db.sequelize.query('INSERT INTO members (member_first, member_last, member_org, member_email, member_pass, member_level) VALUES ($1, $2, $3, $4, $5, $6)', [req.body.first, req.body.last, req.body.org, req.body.email, hash, 0], (err, results) => {
@@ -23,7 +23,7 @@ export.register = (req, res, next) => {
   })
 }
 
-export.login = (req, res, next) => {
+exports.login = (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) { handleResponse(res, 500, 'error'); }
     if (!user) { handleResponse(res, 404, 'User not found'); }
@@ -37,7 +37,7 @@ export.login = (req, res, next) => {
   })(req, res, next);
 }
 
-export.logout = (req, res, next) => {
+exports.logout = (req, res, next) => {
   req.session.destroy();
   handleResponse(res, 200, 'success');
 }
