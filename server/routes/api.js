ef653369bef16e09ba2dcc1cd302596d62ecf660
HashedDan <georgedanielmangum@gmail.com>
2017-12-25 18:56:23 -0600

    updated auth routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const authFuncs = require('../auth/auth-functions');
 const organizations = require('./requests/organizations');
 const members = require('./requests/members');
 const recruits = require('./requests/recruits');
+
+router.get('/login', authFuncs.loginRedirect);
 
 router.get('/organizations', organizations.getAll);
 
---
+++ server/routes/requests/auth.js
@@ -0,0 +1,16 @@
+const db = require('../../models/index');
+const passport = require('../auth/local.js');
+const bcrypt = require('bcryptjs');
+
+export.register = (req, res, next) => {
+
+}
+
+export.login = (req, res, next) => {
+
+}
+
+export.logout = (req, res, next) => {
+
+}
+
